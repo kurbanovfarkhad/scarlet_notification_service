98c0638ac92ce4233629e50b5cd0d79073868830
dg <f.kurbanov@globerce.capital>
2025-09-28 16:50:21 +0500

    init2

--- integration/src/test/java/scarlett/notification/org/integration/MainApplicationTests.java
+++ integration/src/test/java/scarlett/notification/org/integration/MainApplicationTests.java
@@ -1,10 +1,10 @@
-package scarlett.notification.org.scarlett_notification_service;
+package scarlett.notification.org.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ScarlettNotificationServiceApplicationTests {
+class MainApplicationTests {
 
     @Test
     void contextLoads() {
--- main/src/main/java/scarlett/notification/org/main/ScarlettNotificationServiceApplication.java
+++ main/src/main/java/scarlett/notification/org/main/ScarlettNotificationServiceApplication.java
@@ -1,4 +1,4 @@
-package scarlett.notification.org.scarlett_notification_service;
+package scarlett.notification.org.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
