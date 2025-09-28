02deb5de59c5471bd363fc50d2208a5456a5cf01
dg <f.kurbanov@globerce.capital>
2025-09-28 16:04:20 +0500

    init

---
+++ src/main/java/scarlett/notification/org/scarlett_notification_service/ScarlettNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package scarlett.notification.org.scarlett_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScarlettNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScarlettNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scarlett/notification/org/scarlett_notification_service/ScarlettNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package scarlett.notification.org.scarlett_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarlettNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
